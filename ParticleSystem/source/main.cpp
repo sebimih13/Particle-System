15c46523d6b6536598ac35a2bbcff88533de8550
Sebastian Mihalache <sebimih@gmail.com>
2024-10-20 21:23:36 +0300

    Init GLFW

--- ParticleSystem/source/main.cpp
+++ ParticleSystem/source/main.cpp
@@ -1,8 +1,25 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+static constexpr int WIDTH = 800;
+static constexpr int HEIGHT = 600;
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // loop
+    }
 
     return EXIT_SUCCESS;
 }
