3206d03e1a4f7778faea9025f16ca9c5ea0da5d4
Sebastian Mihalache <sebimih@gmail.com>
2024-10-21 20:44:17 +0300

    Install Vulkan SDK for Windows using batch

--- generate_project.bat
+++ generate_project.bat
@@ -1,4 +1,9 @@
 @echo off
+
+set WINDOWS_VULKAN_SDK_INSTALL_URL=https://sdk.lunarg.com/sdk/download/latest/windows/vulkan_sdk.exe
+set WINDOWS_VULKAN_SDK_OUTPUT_FILE=VulkanSDK-Installer.exe
+set VULKAN_SDK_DEFAULT=C:\VulkanSDK
+
 setlocal
 
 :: BatchGotAdmin
@@ -26,8 +31,50 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------
 
+echo Checking for Vulkan SDK
+if defined VULKAN_SDK (
+    echo Vulkan SDK is already installed - %VULKAN_SDK%
+) else (
+    echo Vulkan SDK is not installed
+
+    echo Download installer for Windows
+    curl -o "%WINDOWS_VULKAN_SDK_OUTPUT_FILE%" "%WINDOWS_VULKAN_SDK_INSTALL_URL%"
+
+    echo Run Vulkan SDK Installer
+    "%WINDOWS_VULKAN_SDK_OUTPUT_FILE%" --root %VULKAN_SDK_DEFAULT% --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.vma
+
+    echo Set VULKAN_SDK
+    set VULKAN_SDK=%VULKAN_SDK_DEFAULT%
+
+    echo Remove Vulkan SDK Installer
+    del "%WINDOWS_VULKAN_SDK_OUTPUT_FILE%"
+)
+
+if not defined VULKAN_SDK (
+    echo Rerun the script
+    pause
+    exit
+)
+
+echo Checking for VulkanSDK vendor folder
+if exist vendor\VulkanSDK (
+    echo Found VulkanSDK vendor folder
+) else (
+    echo Create VulkanSDK vendor folder
+    mkdir vendor\VulkanSDK /p
+
+    echo Copy 'include' folder
+    xcopy %VULKAN_SDK%\Include\ %cd%\vendor\VulkanSDK\include /E /I /Y
+
+    echo Copy 'lib' folder
+    xcopy %VULKAN_SDK%\Lib\ %cd%\\vendor\VulkanSDK\lib /E /I /Y
+
+    echo Copy 'glslc.exe'
+    xcopy %VULKAN_SDK%\Bin\glslc.exe %cd%\vendor\VulkanSDK /E /I /Y
+)
+
 REM run premake
-call vendor\premake5\premake5.exe vs2022
+REM call vendor\premake5\premake5.exe vs2022
 
 endlocal
 pause
